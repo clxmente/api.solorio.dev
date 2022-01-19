33651300ae1f81ad97278849e3499c9c20e788b4
clxmente <clem@csu.fullerton.edu>
2022-01-18 18:47:11 -0800

    player_info

---
+++ app/routers/nba/nba.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from app.routers.nba.query_mongo import return_player_info
+
+router = APIRouter(
+  prefix="/nba",
+  tags=["NBA"]
+)
+
+@router.get("/players/")
+async def player_endpoint(player_name: str):
+  return return_player_info(player_name.replace("-", " "))
