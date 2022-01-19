892bcf8bf79ba492c366ca3395851885638f0f7a
clxmente <clem@csu.fullerton.edu>
2022-01-19 11:39:30 -0800

    Player endpoint documentation

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from email import message
+from ensurepip import version
+from sys import api_version
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
+from app.routers.exceptions import DoesNotExist
+
+# import the routers from other modules
+from app.routers.nba import nba
+
+app = FastAPI(
+    title="api.solorio.dev",
+    description="Personal API built for my projects 🚀",
+    version="1.0.0"
+)
+
+app.include_router(nba.router)
+
+@app.exception_handler(DoesNotExist)
+async def player_dne(request: Request, exc: DoesNotExist):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "message": "No record found."
+        }
+    )
+
+@app.get("/")
+async def root():
+    return {"messsage": "hello!"}
--- app/routers/nba/nba.py
+++ app/routers/nba/nba.py
@@ -1,11 +1,27 @@
 from fastapi import APIRouter
+
+from pydantic import BaseModel
 from app.routers.nba.query_mongo import return_player_info
+
+class NBAPlayerResponse(BaseModel):
+    NBA_ID: int
+    FULL_NAME: str
+    JERSEY_NUM: str
+    POS: str
+    TEAM: str
+
+class Message(BaseModel):
+    message: str
 
 router = APIRouter(
     prefix="/nba",
     tags=["NBA"]
 )
 
-@router.get("/players")
+@router.get(
+    "/player/{player_name}", # endpoint,
+    description="Get basic player information from an NBA player",
+    response_model=NBAPlayerResponse, # how the successful response looks
+    responses={404: {"model": Message}})
 async def player_endpoint(player_name: str):
     return return_player_info(player_name.replace("-", " "))
