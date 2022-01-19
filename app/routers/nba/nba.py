120b4d5c03e4230a26943b85fe35a93d7c5553e7
clxmente <clem@csu.fullerton.edu>
2022-01-19 12:00:35 -0800

    team endpoint added w/ documentation

--- app/routers/nba/nba.py
+++ app/routers/nba/nba.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter
+from typing import List
 
 from pydantic import BaseModel
-from app.routers.nba.query_mongo import return_player_info
+from app.routers.nba.query_mongo import return_player_info, return_team_info
 
 class NBAPlayerResponse(BaseModel):
-    NBA_ID: int
-    FULL_NAME: str
-    JERSEY_NUM: str
-    POS: str
-    TEAM: str
+    NBA_ID = 2544
+    FULL_NAME = "LeBron James"
+    JERSEY_NUM = "6"
+    POS = "F"
+    TEAM = "LAL"
+
+class NBATeamPlayersModel(BaseModel):
+    FULL_NAME = "Anthony Davis"
+    JERSEY_NUM = "3"
+    POS = "F-C"
+
+class NBATeamResponse(BaseModel):
+    __root__: List[NBATeamPlayersModel]
 
 class Message(BaseModel):
     message: str
@@ -20,8 +29,18 @@ router = APIRouter(
 
 @router.get(
     "/player/{player_name}", # endpoint,
-    description="Get basic player information from an NBA player",
+    description="Get basic player information from an NBA player.",
     response_model=NBAPlayerResponse, # how the successful response looks
-    responses={404: {"model": Message}})
+    responses={404: {"model": Message}}
+)
 async def player_endpoint(player_name: str):
     return return_player_info(player_name.replace("-", " "))
+
+@router.get(
+    "/team/{team_abbreviation}",
+    description="Get the list of players on an NBA team.",
+    response_model=NBATeamResponse,
+    responses={404: {"model": Message}}
+)
+async def team_endpoint(team_abbreviation: str):
+    return return_team_info(team_abbreviation)
